bbe9cc65b75bb5e86d01de6ab61cc2b39aeb9b6a
Prince Anani Selase <pa.selase12@gmail.com>
2026-05-30 19:14:23 +0200

    feat: VPC network foundation — public/private subnets, IGW, routing

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,26 @@
-
 # outputs.tf — values reported after apply
 
-output "bucket_name" {
-  description = "Name of the created S3 bucket"
-  value       = aws_s3_bucket.demo.bucket
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
 }
 
-output "bucket_arn" {
-  description = "ARN of the created S3 bucket"
-  value       = aws_s3_bucket.demo.arn
+output "public_subnet_id" {
+  description = "ID of the public subnet"
+  value       = aws_subnet.public.id
 }
 
-output "bucket_region" {
-  description = "Region the bucket was created in"
-  value       = aws_s3_bucket.demo.region
+output "private_subnet_id" {
+  description = "ID of the private subnet"
+  value       = aws_subnet.private.id
+}
+
+output "internet_gateway_id" {
+  description = "ID of the internet gateway"
+  value       = aws_internet_gateway.main.id
+}
+
+output "availability_zone" {
+  description = "AZ the subnets are deployed in"
+  value       = aws_subnet.public.availability_zone
 }
